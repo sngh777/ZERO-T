29861e99cc6f4274566962e30d16fc4b602631ba
sngh777 <abhinavsngh777@gmail.com>
2025-02-06 22:23:30 +0530

    Update find_viaSSH.py

--- scripts/find_viaSSH.py
+++ scripts/find_viaSSH.py
@@ -3,7 +3,7 @@ import docker
 import json
 
 # Hardcoded remote EC2 IP address and SSH login details
-REMOTE_HOST = '34.207.71.152'  # Replace with your EC2 public IP
+REMOTE_HOST = '34.207.159.185'  # Replace with your EC2 public IP
 SSH_USER = 'ec2-user'  # Replace with your EC2 username (typically 'ec2-user' for Amazon Linux)
 SSH_KEY_PATH = '/home/user/Downloads/zta.pem'  # Path to your private SSH key
 
